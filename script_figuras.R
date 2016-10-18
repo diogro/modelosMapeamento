b28aa839f3cc129633b3c9dd7d3a4623002eaa52
diogro <diogro@gmail.com>
2016-10-18 15:17:21 -0200

    basic regression plots

--- script_figuras.R
+++ script_figuras.R
@@ -8,6 +8,7 @@ phenotypes = c(1, 0.5, -1)
 p = 0.5; q = 1 - p
 N = 300
 pop_index = c(rep(1, floor(N*p^2)), rep(2, floor(N*2*p*q)), rep(3, floor(N*q^2)))
+pop_index_2 = sample(pop_index)
 pop = phenotypes[pop_index] + 3 + rnorm(N, 0, 0.2)
 medias = ddply(pop_df, .(genótipos), numcolwise(mean))
 pop_df = data.frame(genótipos = factor(genotypes[pop_index], levels = c("SS", "LS", "LL")), 
@@ -16,13 +17,13 @@ pop_df = data.frame(genótipos = factor(genotypes[pop_index], levels = c("SS", "
 set.seed(42)
 anova_plot = ggplot(pop_df, aes(genótipos, Fenótipo)) + geom_jitter(size = 2, width = 0.2) + 
   geom_point(data = medias, size = 5, color = "red")
-print(anova_plot)
+save_plot("apresentacao/images/anova_plot.png", anova_plot, base_height = 5, base_aspect_ratio = 1.3)
 
 set.seed(42)
 centered_anova_plot = ggplot(pop_df, aes(genótipos, Fenótipo)) + geom_jitter(size = 2, width = 0.2) + geom_hline(yintercept = 3) +
   scale_y_continuous(breaks = c(1, 0.5, 0, -1)+3, labels = c("a", "d", "0", "-a"))+ 
   geom_point(data = medias, size = 5, color = "red")
-print(centered_anova_plot)
+save_plot("apresentacao/images/centered_anova_plot.png", centered_anova_plot, base_height = 5, base_aspect_ratio = 1.3)
 
 set.seed(42)
 additive_regression = ggplot(pop_df, aes(genótipos, Fenótipo)) + geom_jitter(size = 2, width = 0.2) +
@@ -31,6 +32,7 @@ additive_regression = ggplot(pop_df, aes(genótipos, Fenótipo)) + geom_jitter(s
   geom_point(data = medias, size = 5, color = "red") + ggtitle("Regressão dos efeitos aditivos") + 
   geom_abline(slope = 1, intercept = 1)
 
+pop_df$additive = ifelse(pop_df$genótipos == "LS", 0, ifelse(pop_df$genótipos=="LL", 1, -1)) 
 pop_df$dominance = ifelse(pop_df$genótipos == "LS",1,0)
 medias_dm = ddply(pop_df, .(dominance), numcolwise(mean))
 dominance_regression = ggplot(pop_df, aes(dominance, Fenótipo)) + 
@@ -42,8 +44,7 @@ dominance_regression = ggplot(pop_df, aes(dominance, Fenótipo)) +
   ggtitle("Regressão dos efeitos de dominância") + 
   geom_abline(slope = 0.5, intercept = 3)
 
-ortigonal_regression = plot_grid(additive_regression, dominance_regression)
-print(ortigonal_regression)
+ortogonal_regression = plot_grid(additive_regression, dominance_regression)
+save_plot("apresentacao/images/ortogonal_regression_plot.png", ortogonal_regression, base_height = 5, base_aspect_ratio = 1.3, ncol = 2)
 
-pop_df$additive = ifelse(pop_df$genótipos == "LS", 0, ifelse(pop_df$genótipos=="LL", 1, -1)) 
 summary(lm(Fenótipo ~ additive + dominance, data = pop_df))
