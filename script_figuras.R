4303ed4bb685add6f71e37d2c0afa8134c097a4e
Diogo Melo <diogro@gmail.com>
2020-07-15 16:24:20 -0300

    coisas novas

--- script_figuras.R
+++ script_figuras.R
@@ -3,56 +3,58 @@ library(cowplot)
 library(plyr)
 
 set.seed(42)
-genotypes = c("LL", "LS", "SS")
-phenotypes = c(1, 0.5, -1)
+genotypes = c("AA", "AB", "BB")
+phenotypes = c(0.1, 0.05, -0.1)
 p = 0.5; q = 1 - p
 N = 300
 pop_index = c(rep(1, floor(N*p^2)), rep(2, floor(N*2*p*q)), rep(3, floor(N*q^2)))
 pop_index_2 = sample(pop_index)
 pop = phenotypes[pop_index] + 3 + rnorm(N, 0, 0.2)
-pop_df = data.frame(genótipos = factor(genotypes[pop_index], levels = c("SS", "LS", "LL")), 
-                    Fenótipo = pop)
-medias = ddply(pop_df, .(genótipos), numcolwise(mean))
+pop_df = data.frame(Genotypes = factor(genotypes[pop_index], levels = c("BB", "AB", "AA")), 
+                    Phenotype = pop)
+medias = ddply(pop_df, .(Genotypes), numcolwise(mean))
 
 
 set.seed(42)
-anova_plot = ggplot(pop_df, aes(genótipos, Fenótipo)) + geom_jitter(size = 2, width = 0.2) + 
+anova_plot = ggplot(pop_df, aes(Genotypes, Phenotype)) + geom_jitter(size = 2, width = 0.2) + 
   geom_point(data = medias, size = 5, color = "red") + 
   theme(
     panel.background = element_rect(fill = "transparent", colour = NA), # or theme_blank()
     plot.background = element_rect(fill = "transparent", colour = NA)
   )
-save_plot("apresentacao/images/anova_plot.png", anova_plot, base_height = 4, base_aspect_ratio = 1.5, bg = "transparent")
+save_plot("~/anova_plot.png", anova_plot, base_height = 4, base_aspect_ratio = 1.5, bg = "transparent")
 
 set.seed(42)
-centered_anova_plot = ggplot(pop_df, aes(genótipos, Fenótipo)) + geom_jitter(size = 2, width = 0.2) + geom_hline(yintercept = 3) +
+centered_anova_plot = ggplot(pop_df, aes(Genotypes, Phenotype)) + geom_jitter(size = 2, width = 0.2) + geom_hline(yintercept = 3) +
   scale_y_continuous(breaks = c(1, 0.5, 0, -1)+3, labels = c("a", "d", "0", "-a"))+ 
   geom_point(data = medias, size = 5, color = "red")+ 
   theme(
     panel.background = element_rect(fill = "transparent", colour = NA), # or theme_blank()
     plot.background = element_rect(fill = "transparent", colour = NA)
   )
-save_plot("apresentacao/images/centered_anova_plot.png", centered_anova_plot, base_height = 4, base_aspect_ratio = 1.5, bg = "transparent")
+save_plot("~/centered_anova_plot.png", centered_anova_plot, base_height = 4, base_aspect_ratio = 1.5, bg = "transparent")
 
 set.seed(42)
-additive_regression = ggplot(pop_df, aes(genótipos, Fenótipo)) + geom_jitter(size = 2, width = 0.2) +
-  scale_y_continuous(breaks = c(1, 0.5, 0, -1)+3, labels = c("a", "d", "0", "-a"))+ 
-  scale_x_discrete(labels = c(-1, 0, 1)) + labs (x = "Genótipos", y = "Fenótipo") +
-  geom_point(data = medias, size = 5, color = "red") + ggtitle("Regressão dos efeitos aditivos") + 
-  geom_abline(slope = 1, intercept = 1)+ 
+additive_regression = ggplot(pop_df, aes(Genotypes, Phenotype)) + geom_jitter(size = 2, width = 0.2) +
+  scale_y_continuous(breaks = c(0.1, 0.0, 0.05, -0.1)+3, labels = c("a", "d", "0", "-a"))+ 
+  scale_x_discrete(labels = c(-1, 0, 1)) + labs (x = "Genotypes", y = "Phenotype") +
+  geom_point(data = medias, size = 5, color = "red") + ggtitle("Additive effects") + 
+  geom_abline(slope = 0.1, intercept = 2.8)+ 
   theme(
     panel.background = element_rect(fill = "transparent", colour = NA), # or theme_blank()
     plot.background = element_rect(fill = "transparent", colour = NA)
   )
+save_plot("~/non_sig_additive_regression.png", additive_regression, base_height = 4, base_aspect_ratio = 1.5, bg = "transparent")
 
-pop_df$additive = ifelse(pop_df$genótipos == "LS", 0, ifelse(pop_df$genótipos=="LL", 1, -1)) 
-pop_df$dominance = ifelse(pop_df$genótipos == "LS",1,0)
+
+pop_df$additive = ifelse(pop_df$Genotypes == "LS", 0, ifelse(pop_df$Genotypes=="LL", 1, -1)) 
+pop_df$dominance = ifelse(pop_df$Genotypes == "LS",1,0)
 medias_dm = ddply(pop_df, .(dominance), numcolwise(mean))
-dominance_regression = ggplot(pop_df, aes(dominance, Fenótipo)) + 
+dominance_regression = ggplot(pop_df, aes(dominance, Phenotype)) + 
   geom_jitter(size = 2, width = 0.1) +
   scale_y_continuous(breaks = c(1, 0.5, 0, -1)+3, labels = c("a", "d", "0", "-a"))+ 
   scale_x_continuous(breaks = c(0, 1))+ 
-  labs (x = "Genótipos", y = "Fenótipo") +
+  labs (x = "Genotypes", y = "Phenotype") +
   geom_point(data = medias_dm, size = 5, color = "red") + 
   ggtitle("Regressão dos efeitos de dominância") + 
   geom_abline(slope = 0.5, intercept = 3)+ 
@@ -64,7 +66,7 @@ dominance_regression = ggplot(pop_df, aes(dominance, Fenótipo)) +
 ortogonal_regression = plot_grid(additive_regression, dominance_regression)
 save_plot("apresentacao/images/ortogonal_regression_plot.png", ortogonal_regression, base_height = 4, base_aspect_ratio = 1, ncol = 2, bg = "transparent")
 
-summary(lm(Fenótipo ~ additive + dominance, data = pop_df))
+summary(lm(Phenotype ~ additive + dominance, data = pop_df))
 
 list_pkgs <- c("plyr", "dplyr", "tidyr", "readr", "car", "ggplot2", "cowplot")
 new_pkgs <- list_pkgs[!(list_pkgs %in% installed.packages()[,"Package"])]
